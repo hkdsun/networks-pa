464ee2a46786a1cef33f6d0a6a0ee2fc98becd7a
djole <djordje_7@hotmail.com>
2015-11-15 19:59:54 -0500

    TODO: -format spaces -identify all necessary parameters for simulation (comp is missing some that it needs) -ppersistent algorithm not done that shit is confusing what are slots -wth is Tp for expBackoff -random wait (not in backoff) how are we going to do it, just a randint() range? if so to what? -identify finish time for packets, or how long they take to process

--- lab2/lan.py
+++ lab2/lan.py
@@ -7,8 +7,10 @@ from computer import Computer
 class Simulator:
 
 	CDmethods = { 'n-p' : nonPersistent
-				  'p-p' : pPersistent }
+                      'p-p' : pPersistent }
 	Kmax = 10
+        #wtf is Tp.. '512' bit time'
+        Tp = 2 #2 is random means nothing
 
 	def __init__(self,numComputers,arrivalRate,speedLAN,persistence,packetLen,runTime):
 		self.network = Network(numComputers,arrivalRate,speedLan)
@@ -18,68 +20,63 @@ class Simulator:
 		self.curTime = 0
 
 	class Network:
-		def __init__(self,N,A,W,P):
-			self.comps = [Computer() for computer in range(N)]
-			self.A = arrivalRate
-			self.W = speedLan
-
-			self.busy = "IDLE"
+            def __init__(self,N,A,W,P):
+	        self.comps = [Computer(self) for computer in range(N)]
+		self.A = arrivalRate
+		self.W = speedLan
+		self.busy = "IDLE"
 	
-	class Computer:
-		def __init__(self,sendTime=0,finishTime):
-			#import object from lab1 / integration
-			self.Q = []
-			self.waitingORsending = 0
-			self.sendTime = sendTime
-			self.collisions = 0
-			self.finishTime = finishTime
-
 	def simulate(self):
-		while(self.curTime != self.runTime):
-			working = list(filter(lambda x: x.waitingORsending == 1, self.comps))
-			if (len(working)) ==  1):
-				self.network.busy = "BUSY"
-			elif (len(working) > 1):
-				self.network.busy = "MULTIPLE"
-			else:
-				self.network.busy = "IDLE"
+	    while(self.curTime != self.runTime):
+                working = list(filter(lambda x: x.waitingORsending == 1, self.comps))
+                if (len(working)) ==  1):
+                    self.network.busy = "BUSY"
+                elif (len(working) > 1):
+                    self.network.busy = "MULTIPLE"
+                else:
+                    self.network.busy = "IDLE"
+                    for comp in self.network.comps:
+                        comp.newPacket()
+                        #collision detection based on persistence
+                        if comp.Q: self.P(comp)
+                self.curTime+=1
 
-			for comp in self.network.comps:
-				#packets sent to Q here, integrate lab1
-				comp.receivePackets()
-				#collision detection based on persistence
-				self.P(comp)
-			self.curTime+=1
+	def nonPersistent(self,comp): 
+            #in the process of sending
+            if (comp.waitingORsending == 1):
+                    if (self.network.busy == "COLLISION"):
+                            comp.collisions+=1
+                            #need to consider how error is done given collisons, could do collis > Kmax check before
+                            comp.sendTime = self.curTime + exponentialBackoff(comp.collisions,Kmax)
+                            comp.waitingORsending = 0
+                    else:
+                            if (self.curTime == comp.finishTime):
+                                    comp.Q.popc(0) 
+                                    comp.waitingORsending = 0
+                            else: pass
+            #waiting for network TO send
+            else:
+                    #random wait done try to send
+                    if (comp.sendTime <= self.curTime):
+                            if (self.network.busy == "IDLE"):
+                                    comp.waitingORsending = 1
+                                    comp.finishTime = self.curTime + packetProcessTime()
+                            else:
+                                    comp.sendTime = self.curTime + randomwait()
+                    #not ur time yet lil packet
+                    else: pass
 
-	def nonPersistent(self,comp):
-		#there are packets to be sent
-		if comp.Q:
-			#in the process of sending
-			if (comp.waitingORsending == 1):
-				if (self.network.busy == "COLLISION"):
-					comp.collisions+=1
-					#need to consider how error is done given collisons, could do collis > Kmax check before
-					comp.sendTime = self.curTime + exponentialBackoff(comp.collisions,Kmax)
-					comp.waitingORsending = 0
-				else:
-					if (self.curTime = comp.finishTime):
-						comp.Q.pop(0) 
-						comp.waitingORsending = 0
-					else: pass
-			#waiting for network TO send
-			else:
-				#random wait done try to send
-				if (comp.sendTime <= self.curTime):
-					if (self.network.busy == "IDLE"):
-						comp.waitingORsending = 1
-						comp.finishTime = self.curTime + packetProcessTime()
-					else:
-						comp.sendTime = self.curTime + randomwait()
-				#not ur time yet lil packet
-				else: pass
-		#no packets in Q
-		else: pass		
+        def pPersistent(self,comp):
+            pass
 
+        def expBackoff(self,comp):
+            if comp.collisions > Kmax:
+                error()
+            else:
+                randTime = randint(0,(2**comp.collisions)-1)
+                #wtf is tp
+                comp.sendTime = randTime*Tp
+            
 
 def main(args):
 	#TODO: decide what arguments needed
