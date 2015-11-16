9bad054685268ee479ccc977fae4abd16609cf6f
Hormoz K <hkheradm@gmail.com>
2015-11-16 05:17:23 -0500

    I had to flatten the nested classes because:
    I couldn't get access to the parent instance. for example in the
    initializer for Generator I couldn't access Packet

--- lab2/computer.py
+++ lab2/computer.py
@@ -1,15 +1,16 @@
 from numpy import random
 from math import ceil
 
-class Computer:
-    def __init__(self,simulator):
 
-        #internal packet stuff
-        self.generator = Generator(simulator)  
+class Computer:
+
+    def __init__(self, simulator):
+        # internal packet stuff
+        self.generator = self.Generator(simulator)
         self.packets = []
         self.Q = []
 
-        #collision detection stuff
+        # collision detection stuff
         self.waitingORsending = 0
         self.sendTime = 0
         self.collisions = 0
@@ -20,33 +21,39 @@ class Computer:
         packet = self.generator.next()
         if packet:
             self.Q.append(packet)
-        else: pass
+        else:
+            pass
 
-    class Generator:
-        def __init__(self, simulator):
-            self.lamb = simulator.lambdaa
-            self.curTime = simulator.curTime
-            self.multiplier = simulator.multiplier
-            self.nextPacket = ceil(random.exponential(1/self.lamb, None) * self.multiplier)
 
-        def next(self):
-            p = None
-            if self.nextPacket == self.curTime:
-                p = Packet(self.curTime)
-                num = ceil(random.exponential(1/self.lamb, None) * self.multiplier)
-                self.nextTime = self.curTime + num
-            self.curTime += 1
-            return p
+class Packet:
 
-    class Packet:
-        def __init__(self, init_tick):
-            self.generate_init_tick = init_tick
-            self.service_init_tick = init_tick
-            self.service_finish_tick = init_tick
-            self.service_started = False
-            self.service_done = False
-            self.lost = False
+    def __init__(self, init_tick):
+        self.generate_init_tick = init_tick
+        self.service_init_tick = init_tick
+        self.service_finish_tick = init_tick
+        self.service_started = False
+        self.service_done = False
+        self.lost = False
 
-        def delay(self):
-            return (self.service_finish_tick - self.generate_init_tick)
+    def delay(self):
+        return (self.service_finish_tick - self.generate_init_tick)
 
+
+class Generator:
+
+    def __init__(self, simulator):
+        self.lamb = simulator.lambdaa
+        self.curTime = simulator.curTime
+        self.multiplier = simulator.multiplier
+        self.nextPacket = ceil(random.exponential(
+            1 / self.lamb, None) * self.multiplier)
+
+    def next(self):
+        p = None
+        if self.nextPacket == self.curTime:
+            p = Packet(self.curTime)
+            num = ceil(random.exponential(
+                1 / self.lamb, None) * self.multiplier)
+            self.nextTime = self.curTime + num
+        self.curTime += 1
+        return p
--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,42 +1,44 @@
-from numpy import random
-from math import ceil
+from numpy.random import randint
 import argparse
-#from progressbar import ProgressBar
 from computer import Computer
+
+
+def secToTicks(secs, tickLength):
+    return secs * tickLength
+
+
+class Network:
+    def __init__(self, simulator, N, A, W, P):
+        self.comps = [Computer(simulator) for _ in range(N)]
+        self.A = simulator.arrivalRate
+        self.W = simulator.speedLan
+        self.busy = "IDLE"
+
 
 class Simulator:
     Kmax = 10
     Tp = 0.00000005
     propTime = 0
 
-    def __init__(self,numComputers,arrivalRate,speedLAN,persistence,packetLen,totalTicks,tickLength,lambdaa):
+    def __init__(self, numComputers, arrivalRate, speedLAN, persistence, packetLen, totalTicks, tickLength, lambdaa):
 
-        CDmethods = { 'n-p' : nonPersistent,
-                      'p-p' : pPersistent }
+        CDmethods = {'n-p': nonPersistent,
+                     'p-p': pPersistent}
 
-        self.network = Network(numComputers,arrivalRate,speedLan)
+        self.network = Network(numComputers, arrivalRate, speedLan)
         self.P = CDmethods[persistence]
         self.L = packetLen
         self.runTime = totalTicks
         self.curTime = 0
         self.tickLength
         global propTime
-        propTime = secToTicks(Tp,tickLength)
+        propTime = secToTicks(self.Tp, tickLength)
 
-    def secToTicks(secs,tickLength):
-        return secs*tickLength
-
-    class Network:
-        def __init__(self,N,A,W,P):
-            self.comps = [Computer(self) for computer in range(N)]
-            self.A = arrivalRate
-            self.W = speedLan
-            self.busy = "IDLE" 
-	
     def simulate(self):
         while(self.curTime != self.runTime):
-            visibleWorkers = list(filter(lambda x: (x.waitingORsending == 1) and (self.curTime - comp.sendTime >= propTime) , self.comps))
-            if (len(visibleWorkers) ==  1):
+            visibleWorkers = list(filter(lambda x: (x.waitingORsending == 1) and (
+                self.curTime - comp.sendTime >= propTime), self.comps))
+            if (len(visibleWorkers) == 1):
                 self.network.busy = "BUSY"
             elif (len(visibleWorkers) > 1):
                 self.network.busy = "MULTIPLE"
@@ -44,94 +46,103 @@ class Simulator:
                 self.network.busy = "IDLE"
             for comp in self.network.comps:
                 comp.newPacket()
-                #collision detection based on persistence
-                if comp.Q: self.P(comp)
-            self.curTime+=1
+                # collision detection based on persistence
+                if comp.Q:
+                    self.P(comp)
+            self.curTime += 1
 
-    def nonPersistent(self,comp): 
-        #in the process of sending
+    def nonPersistent(self, comp):
+        # in the process of sending
         if (comp.waitingORsending == 1):
-                if (self.network.busy == "MULTIPLE"):
-                    collisionWork(comp)                                 
-                else:
-                        if (self.curTime == comp.finishTime):
-                                comp.Q.popc(0) 
-                                comp.waitingORsending = 0
-                        else: pass
-        #waiting for network TO send
-        else:
-                #random wait done try to send
-                if (comp.sendTime <= self.curTime):
-                        if (self.network.busy == "IDLE"):
-                                comp.waitingORsending = 1
-                                #TODO: convert to proper ticks, this is how long it takes package to send
-                                comp.finishTime = self.curTime + secToTicks(self.L/self.network.W)
-                        else:
-                                #TODO: check this random time makes sense lol 
-                                #so it chooses a random time between 1 to time to complete an entire packet
-                                comp.sendTime = self.curTime + randint(1,secToTicks(self.L/self.network.W))
-                #not ur time yet lil packet
-                else: pass
-
-    def pPersistent(self,comp):
-        if comp.waitingORsending == 1:
-            if self.network.busy == "MULTIPLE":
-                collisionWork(comp)                         
+            if (self.network.busy == "MULTIPLE"):
+                self.collisionWork(comp)
             else:
                 if (self.curTime == comp.finishTime):
-                    comp.Q.popc(0) 
+                    comp.Q.popc(0)
                     comp.waitingORsending = 0
-        else:               
-            if (self.network.busy == "IDLE"):
-                if randint(1,100)<= probSEND*100:
+                else:
+                    pass
+        # waiting for network to send
+        else:
+            # random wait done try to send
+            if (comp.sendTime <= self.curTime):
+                if (self.network.busy == "IDLE"):
                     comp.waitingORsending = 1
-                    comp.finishTime = self.curTime + secToTicks(self.L/self.network.W)
+                    # TODO: convert to proper ticks, this is how long it takes
+                    # package to send
+                    comp.finishTime = self.curTime + \
+                        secToTicks(self.L / self.network.W)
+                else:
+                    # TODO: check this random time makes sense lol
+                    # so it chooses a random time between 1 to time to complete
+                    # an entire packet
+                    comp.sendTime = self.curTime + \
+                        randint(1, secToTicks(self.L / self.network.W))
+            # not ur time yet lil packet
+            else:
+                pass
+
+    def pPersistent(self, comp, probSend):
+        if comp.waitingORsending == 1:
+            if self.network.busy == "MULTIPLE":
+                self.collisionWork(comp)
+            else:
+                if (self.curTime == comp.finishTime):
+                    comp.Q.popc(0)
+                    comp.waitingORsending = 0
+        else:
+            if (self.network.busy == "IDLE"):
+                if randint(1, 100) <= probSend * 100:
+                    comp.waitingORsending = 1
+                    comp.finishTime = self.curTime + \
+                        secToTicks(self.L / self.network.W)
                     comp.pState = 0
                 else:
-                    #add wait for slot
-                    if comp.pState: 
-                        comp.sendTime+= secToTicks(self.L/self.network.W)
+                    # add wait for slot
+                    if comp.pState:
+                        comp.sendTime += secToTicks(self.L / self.network.W)
                     else:
-                        #TODO: it says to do exp backoff but with no collisions wtf is that then ?????
-                        comp.sendTime+= randInt(1,1000)
-                    comp.pState ^=1
-                        
-    def collisionWork(self,comp):   
-        comp.collisions+=1
-        #expbackoff pops and sets packet in case of error
-        notError = expBackoff(comp)
+                        # TODO: it says to do exp backoff but with no collision
+                        comp.sendTime += randint(1, 1000)
+                    comp.pState ^= 1
+
+    def collisionWork(self, comp):
+        comp.collisions += 1
+        # expbackoff pops and sets packet in case of error
+        notError = self.expBackoff(comp)
         if notError:
             comp.sendTime = self.curTime + notError
-        comp.waitingORsending = 0      
+        comp.waitingORsending = 0
 
-    def expBackoff(self,comp):
-        if comp.collisions > Kmax:
+    def expBackoff(self, comp):
+        if comp.collisions > self.Kmax:
             comp.q[0].lost = True
             comp.q.pop(0)
             return True
         else:
-            randTime = randint(0,(2**comp.collisions)-1)
-            #TODO
-            comp.sendTime = randTime*propTime
+            randTime = randint(0, (2**comp.collisions) - 1)
+            # TODO
+            comp.sendTime = randTime * propTime
             return False
+
 
 def main(args):
     sim = Simulator(args)
     sim.simulate()
-	
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(__file__, description="A simulator for a M/D/1 and M/D/1/K network queueing model")
     parser.add_argument("--numComputers", "-N", help="Number of computers", type=int, required=True)
     parser.add_argument("--arrivalRate", "-A", help="packets per second", type=int, required=True)
-    parser.add_argument("--speedLAN", "-W", help="bits per second", type=int, required=True) 
+    parser.add_argument("--speedLAN", "-W", help="bits per second", type=int, required=True)
     parser.add_argument("--persistence", "-P", help="non-persistent: n-p or p-persistent: p-p", type=str, required=True)
     parser.add_argument("--lambd", "-l", help="Average number of packets generated /arrived (packets per second)", type=float, required=True)
     parser.add_argument("--ticks", "-t", help="Number of ticks that the simulator should run for", type=int, required=True)
     parser.add_argument("--packet-size", "-L", help="Length of a packet in bits", type=int, required=True)
-    #TODO: how was this used before
+    # TODO: how was this used before
     parser.add_argument("--service-time", "-C", help="The service time received by a packet in bits per second", type=int, required=False)
-    parser.add_argument("--tickLength", "-T", help="tick to second ratio", type=int,default=10)
+    parser.add_argument("--tickLength", "-T", help="tick to second ratio", type=int, default=10)
     parser.add_argument("--data-points", "-M", help="Number of times the simulation should run and values be averaged out", type=int, default=int(5))
     args = parser.parse_args()
-    args = (args.numComputers,args.arrivalRate,args.speedLAN,args.persistence,args.packet_size,args.ticks,args.tickLength,args.lambd)
-    main(args)	    
+    args = (args.numComputers, args.arrivalRate, args.speedLAN, args.persistence, args.packet_size, args.ticks, args.tickLength, args.lambd)
+    main(args)
